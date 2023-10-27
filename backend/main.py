ed6559eca89f84d4d40363684a1ee60c8bdc1c4d
Xu <deep.work.max@gmail.com>
2023-10-27 16:42:56 +0800

    add allow_unsafe_werkzeug

--- backend/main.py
+++ backend/main.py
@@ -28,4 +28,4 @@ def handle_message(message):
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5001)
+    socketio.run(app, host='0.0.0.0', port=5001,allow_unsafe_werkzeug=True)
